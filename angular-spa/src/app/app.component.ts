1144ab2327ecb6f9360367db759b3b50d175c232
Tamas Szucs <tomfurrier@gmail.com>
2018-06-24 23:25:56 +0200

    scroll to top on route change

--- angular-spa/src/app/app.component.ts
+++ angular-spa/src/app/app.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from './auth/auth.service';
+import { Router, NavigationEnd } from '@angular/router';
+import {
+  BreakpointObserver,
+  BreakpointState,
+  Breakpoints
+} from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,11 +14,29 @@ import { AuthService } from './auth/auth.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  constructor(private authService: AuthService) {}
+  window: Element;
+  isHandset: Observable<BreakpointState> = this.breakpointObserver.observe(
+    Breakpoints.Handset
+  );
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private breakpointObserver: BreakpointObserver
+  ) {}
 
   onToggle() {}
 
   ngOnInit() {
     this.authService.initAuthListener();
+
+    this.router.events.subscribe(event => {
+      // Scroll to top if accessing a page, not via browser history stack
+      if (event instanceof NavigationEnd) {
+        const contentContainer =
+          document.querySelector('.mat-sidenav-content') || this.window;
+        contentContainer.scrollTo(0, 0);
+      }
+    });
   }
 }
